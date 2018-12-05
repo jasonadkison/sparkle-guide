b7e354407a6e7cdddb16a675f9624fb19e7a337f
Jason Adkison <jadkison@gmail.com>
2018-12-05 12:06:37 -0600

    adds another ec2 instance resource

--- sparkleformation/compute.rb
+++ sparkleformation/compute.rb
@@ -1,3 +1,4 @@
-SparkleFormation.new(:compute, :provider => :aws).load(:base).overrides do
+SparkleFormation.new(:compute, provider: :aws).load(:base).overrides do
   dynamic!(:node, :sparkle)
+  dynamic!(:node, :unicorn)
 end
